dc728a633eeb320af9b4d6e88ffd18e41be7f1a6
Vladislav Bakaev <vladislav-bakaev@mail.ru>
2018-03-02 20:08:17 +0400

    Добавлен каркас чат-бота Telegram

--- src/main/scala/com/gifty/GiftyBot.scala
+++ src/main/scala/com/gifty/GiftyBot.scala
@@ -2,8 +2,41 @@ package com.gifty
 
 import info.mukel.telegrambot4s.api.{Polling, TelegramBot}
 import info.mukel.telegrambot4s.api.declarative.{Callbacks, Commands}
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{Config, ConfigFactory}
 
 object GiftyBot extends TelegramBot with Polling with Commands with Callbacks {
-  lazy val token: String = ConfigFactory.load().getString("telegram.token")
+  lazy val config: Config = ConfigFactory.load()
+  lazy val token: String = config.getString("telegram.token")
+
+  onCommand(config.getString("bot.command.start")) { implicit msg =>
+
+  }
+
+  onCommand(config.getString("bot.command.add_question")) { implicit msg =>
+
+  }
+
+  onCommand(config.getString("bot.command.help")) { implicit msg =>
+
+  }
+
+  onCallbackWithTag(config.getString("bot.button.yes.tag")) { implicit cbq =>
+
+  }
+
+  onCallbackWithTag(config.getString("bot.button.no.tag")) { implicit cbq =>
+
+  }
+
+  onCallbackWithTag(config.getString("bot.button.not_know.tag")) { implicit cbq =>
+
+  }
+
+  onCallbackWithTag(config.getString("bot.button.accepted.tag")) { implicit cbq =>
+
+  }
+
+  onCallbackWithTag(config.getString("bot.button.rejected.tag")) { implicit cbq =>
+
+  }
 }
